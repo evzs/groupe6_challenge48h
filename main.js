143b2b31f395fcb88e8b244755eacf35aedf5905
evzs <szve@proton.me>
2023-02-07 08:56:06 +0100

    page morse + hexa

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function op(){
-    var field2=document.getElementById("input-field2").value;
-   if(field2==="test")
-       document.location.href="/index.html"
-       
-    else{
+document.addEventListener("keydown", function (event) {
+
+    if (event.ctrlKey) {
+
+        event.preventDefault();
+
+    }
+
+    if (event.keyCode == 123) {
+
+        event.preventDefault();
+
+    }
+
+});
+
+document.addEventListener('contextmenu',
+    event => event.preventDefault()
+);
+
+function op() {
+    var field2 = document.getElementById("input-field2").value;
+    if (field2 === "test")
+        document.location.href = "/index.html"
+
+    else {
         alert("invaild information")
-   }
+    }
 }  
